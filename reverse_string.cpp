3bfdf2beae64fd81518556b32529be3e8e6263bb
Pranav <pranav.techiegeek@gmail.com>
2023-11-02 08:42:30 +0530

    ready

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -1,5 +1,11 @@
 #include "reverse_string.h"
 
-namespace reverse_string {
+namespace reverse_string
+{
 
-}  // namespace reverse_string
+std::string reverse_string(const std::string& text)
+{
+    // write your code here
+}
+
+}
